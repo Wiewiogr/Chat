50b4ddea9c41d5efd52d484ed075b62f2ce88bc5
Wiewiogr <wiewiora.tomasz1@gmail.com>
2017-07-23 17:51:17 +0200

    add User and UserRepository

--- msgService/src/main/java/pl/wiewiogr/msgService/MsgServiceApplication.java
+++ msgService/src/main/java/pl/wiewiogr/msgService/MsgServiceApplication.java
@@ -1,11 +1,12 @@
 package pl.wiewiogr.msgService;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsgServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsgServiceApplication.class, args);
 	}
---
+++ msgService/src/test/java/pl/wiewiogr/msgService/UserTest.java
@@ -0,0 +1,48 @@
+package pl.wiewiogr.msgService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.testng.Assert.*;
+
+@SpringBootTest(classes = MsgServiceApplication.class)
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+public class UserTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void shouldFindUsers(){
+        //given
+        User tom = new User();
+        tom.setName("Tomasz");
+        User aga = new User();
+        aga.setName("Aga");
+        userRepository.save(tom);
+        userRepository.save(aga);
+
+        //when
+        User firstUser = userRepository.findOne(1L);
+        Iterable<User> allUsers = userRepository.findAll();
+
+        //then
+
+        assertThat(firstUser.getName())
+                .isEqualTo("Tomasz");
+
+        assertThat(allUsers)
+                .extracting("name")
+                .contains("Tomasz")
+                .contains("Aga");
+    }
+
+}
